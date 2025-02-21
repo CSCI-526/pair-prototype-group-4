d7984e6efee837ecaa61e75cb9d3d3544f2d06cd
Abby Farhat <abbysfarhat@gmail.com>
2025-02-20 22:27:38 -0800

    fix: bombs not exploding breakables

--- 526 Pair Prototype/Assets/Scripts/Bomb.cs
+++ 526 Pair Prototype/Assets/Scripts/Bomb.cs
@@ -4,6 +4,17 @@ public class Bomb : MonoBehaviour
 {
     public Player player;
 
+    // on collision with player, explode
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Breakable breakable = other.gameObject.GetComponent<Breakable>();
+        if (breakable != null)
+        {
+            breakable.DestroyBreakable();
+            Explode();
+        }
+    }
+
     public void Explode()
     {
         Vector2 direction = ((Vector2)player.transform.position - (Vector2)transform.position).normalized;
