d79e2575e874179a503c5ec2f49a0e3d5baa2da9
Abby Farhat <abbysfarhat@gmail.com>
2025-02-21 09:20:14 -0800

    chore: removed spaces from project folder name

--- 526PairPrototype/Assets/Scripts/Bomb.cs
+++ 526PairPrototype/Assets/Scripts/Bomb.cs
@@ -13,17 +13,6 @@ public class Bomb : MonoBehaviour
         }
     }
 
-    // on collision with player, explode
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        Breakable breakable = other.gameObject.GetComponent<Breakable>();
-        if (breakable != null)
-        {
-            breakable.DestroyBreakable();
-            Explode();
-        }
-    }
-
     public void Explode()
     {
         Vector2 direction = ((Vector2)player.transform.position - (Vector2)transform.position).normalized;
